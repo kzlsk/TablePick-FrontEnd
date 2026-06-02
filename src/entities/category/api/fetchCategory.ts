663a2fca7fb8fcbd1271f2b7cd08bd0e2694b764
kzlsk <rlaflsk4887@gmail.com>
2026-06-02 15:36:44 +0900

    feat : 카테고리 API 호출 로직을 Supabase로 전환

--- src/entities/category/api/fetchCategory.ts
+++ src/entities/category/api/fetchCategory.ts
@@ -1,7 +1,15 @@
-import api from "@/@shared/api/api";
 import { CategoryProps } from "../types/categoryType";
+import { supabase } from "@/@shared/types/supabase";
 
 export const fetchCategory = async (): Promise<CategoryProps[]> => {
-    const { data } = await api.get('/api/category');
-    return data;
-}
+  const { data, error } = await supabase
+    .from("categories")
+    .select("id, name")
+    .order("id", { ascending: true });
+
+  if (error) {
+    console.error("카테고리 데이터를 불러오지 못했습니다. : ", error);
+    return [];
+  }
+  return data || [];
+};
